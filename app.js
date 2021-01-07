e33a43f7fe0280a89c73b77bf1c8f829661723b7
Deborah Adebiyi <deborahadebiyi@BGL-MAC-026367.bgl.financial.local>
2021-01-07 15:30:50 +0000

    created healthcheck endpoint returns 200 status code

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
     res.send('Hello World');
 })
 
+app.get('/health', function(req,res){
+    res.status(200).send('OK');
+})
+
 app.listen(3000, function(){
     console.log('App listening on localhost:3000');
 })
