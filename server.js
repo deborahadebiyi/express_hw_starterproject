51bf0c13a5995e13c4d7b47a566bb2f7d138cd26
deborahadebiyi <16781923+deborahadebiyi@users.noreply.github.com>
2021-01-19 15:02:52 +0000

    adding database with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const app = require("./app");
-// const api = require("./api");
+require('dotenv').config();
 
-app.listen(3000);
+const app = require("./app");
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connected');
+});
+
+app.listen(3000, () => console.log('Server started'));
 
 //server is separated from the app so it listens to the port after testing
